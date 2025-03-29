657e511dfe9c39483ac323acebbb4b86994273e0
AdelineG218 <greene.adeline08@gmail.com>
2025-03-29 14:04:24 -0600

    fix merge conflict

--- src/app/upload/page.js
+++ src/app/upload/page.js
@@ -1,49 +1,4 @@
 "use client";
-<<<<<<< HEAD
- 
- import Image from "next/image";
- import styles from "../page.module.css";
- import Link from "next/link";
- import { CldUploadWidget } from "next-cloudinary";
- import { useState } from "react";
- 
- export default function Home() {
-   const [url, setUrl] = useState("nothing");
-   return (
-     <div className={styles.page}>
-       <main className={styles.main}>
-         <h1>Upload 2-5 Images</h1>
-
-         <CldUploadWidget
-           uploadPreset="user-image-upload"
-           onSuccess={(result) => {
-             console.log("the image upload worked yay");
-             console.log("secure url: ", result.info.secure_url);
-             setUrl(result.info.secure_url);
-           }}
-         >
-           {({ open }) => {
-             return <button onClick={() => open()}>Upload an Image</button>;
-           }}
-         </CldUploadWidget>
-         <h1>{url}</h1>
-         <div className={styles.ctas}>
-           <Link href="/matches" className={styles.primary}>
-             <Image
-               className={styles.globe}
-               src="/globe.svg"
-               alt="Vercel logomark"
-               width={20}
-               height={20}
-             />
-             Get your matches!
-           </Link>
-         </div>
-       </main>
-     </div>
-   );
- }
-=======
 
 import Image from "next/image";
 import styles from "../page.module.css";
@@ -86,4 +41,3 @@ export default function Home() {
     </div>
   );
 }
->>>>>>> a2fb31983f9cacdf6f8b9377f583a8e89da229f9
