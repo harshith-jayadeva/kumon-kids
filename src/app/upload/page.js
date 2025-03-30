d2f86db79eeae276c91a2e6f7b88101dd55afd04
Harshith Jayadeva <harshithjayadeva@gmail.com>
2025-03-30 00:22:10 -0600

    created the form

--- src/app/upload/page.js
+++ src/app/upload/page.js
@@ -3,10 +3,61 @@
 import styles from "../page.module.css";
 import Link from "next/link";
 import { CldUploadWidget } from "next-cloudinary";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 import { db } from "../../../firebaseConfig";
 import { doc, setDoc } from "firebase/firestore";
+
+const ExpandableBioText = ({ bioRef }) => {
+  return (
+    <textarea
+      className={styles.bio}
+      ref={bioRef}
+      placeholder="Write your bio here..."
+      style={{
+        width:"97%",
+        gap: '10px',
+        height: '240px',
+        resize: "none",
+      }}
+    />
+  );
+};
+
+const ExpandableNameText = ({ firstNameRef }) => {
+  return (
+    <textarea
+      className={styles.names}
+      ref={firstNameRef}
+      placeholder="Write your first name here..."
+      style={{
+        padding: '10px',
+        width:"95%",
+        flex: '1',
+        height: '40px',
+        resize: "none",
+      }}
+    />
+  );
+};
+
+const ExpandableLastNameText = ({ lastNameRef}) => {
+  return (
+    <textarea
+      className={styles.names}
+      ref={lastNameRef}
+      placeholder="Write your last name here..."
+      style={{
+        padding: '10px',
+        width: '95%',
+        flex: '1',
+        height: '40px',
+        resize: "none",
+      }}
+    />
+  );
+};
+
 
 const uploadData = async (collectionName, id, data) => {
   try {
@@ -19,6 +70,29 @@ const uploadData = async (collectionName, id, data) => {
 
 export default function Home() {
   const [urlList, setUrlList] = useState([]);
+  const firstNameRef = useRef(null);
+  const lastNameRef = useRef(null);
+  const bioRef = useRef(null);
+
+  const submit = () => {
+    const firstName = firstNameRef.current ? firstNameRef.current.value:"";
+    const lastName = lastNameRef.current ? lastNameRef.current.value:"";
+    const bio = bioRef.current ? bioRef.current.value:"";
+    const fullName = `${firstName} ${lastName}`.trim();
+  
+
+    const data = {
+      name: fullName, 
+      lastName: lastName,
+      bio: bio,
+      image_urls: urlList,
+    };
+
+    console.log(data);
+
+    const userId = "user-" + Date.now();
+    //uploadData("users", userId, data);
+  };
 
   return (
     <div className={styles.page}>
@@ -52,16 +126,36 @@ export default function Home() {
             style={{ maxWidth: "1000px", maxHeight: "auto" }}
           />
         )}
+        <div className={styles.inputArea}>
+          <div className={styles.nameArea}>
+            <div className={styles.firstName}>
+              <h3>First Name</h3>
+              <ExpandableNameText firstNameRef={firstNameRef}/>
+            </div>
+
+            <div className={styles.lastName}>
+              <h3>Last Name</h3>
+              <ExpandableLastNameText lastNameRef={lastNameRef}/>
+            </div>
+
+          </div>
+          
+          <h3>List your hometown, interests, and hobbies!</h3>
+          <ExpandableBioText bioRef={bioRef}/>
+        </div>
+      
         <div
           className={styles.button}
-          onClick={() => {
-            const data = {
-              name: "John Pork",
-              bio: "hi my name is john pork",
-              image_urls: urlList,
-            };
-            uploadData("users", "exampleuser1", data);
-          }}
+          // onClick={() => {
+          //   const data = {
+          //     name: "John Pork",
+          //     bio: "hi my name is john pork",
+          //     image_urls: urlList,
+          //   };
+          //   uploadData("users", "exampleuser1", data);
+          // }}
+
+          onClick={submit}
         >
           <Link href="/matches" className={styles.primary}>
             Get your matches!
